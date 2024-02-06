b30c0066b810876af237acb441a1f67917687593
Rus7Iv <rusivary@gmail.com>
2024-02-06 15:16:19 +0400

    added Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <>
+      <Header />
       <h1>Hello world!</h1>
     </>
   );
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import './Header.styles.css';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <h1 className="title">Puzzle Editor</h1>
+    </header>
+  );
+};
+
+export default Header;
