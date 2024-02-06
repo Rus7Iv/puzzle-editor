45a1fcd3c01c2af005d20140644a3a964b6e69b7
Rus7Iv <rusivary@gmail.com>
2024-02-06 15:50:38 +0400

    added Footer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
+import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 
 function App() {
   return (
     <>
       <Header />
-      <h1>Hello world!</h1>
+      <div className="content">
+        <h1>Hello world!</h1>
+      </div>
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import './Footer.styles.css';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p className="footer-text">© 2024 Омегасофтвер Рус</p>
+    </footer>
+  );
+};
+
+export default Footer;
