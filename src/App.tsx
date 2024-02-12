e463f1508a4998db2d635e86720bef33f5badba2
Rus7Iv <rusivary@gmail.com>
2024-02-12 14:42:47 +0400

    change App

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,9 @@
-import { useState, useEffect } from 'react';
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import ImageUploader from "./components/ImageUpload/ImageUpload";
 import PuzzleBoard from "./components/PuzzleBoard/PuzzleBoard";
 
 function App() {
-  const [isImageUploaded, setImageUploaded] = useState(false);
-
-  useEffect(() => {
-    const checkImageUpload = () => {
-      for (let i = 0; i < 3; i++) {
-        for (let j = 0; j < 3; j++) {
-          if (!localStorage.getItem(`image_part_${i}_${j}`)) {
-            return false;
-          }
-        }
-      }
-      return true;
-    };
-
-    setImageUploaded(checkImageUpload());
-  }, []);
-
   return (
     <>
       <Header />
